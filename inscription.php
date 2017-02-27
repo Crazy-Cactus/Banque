77a238a297e1225ade16ca46c626547c41840798
Crazy_Cactus <Mathias.Genesis@Gmail.com>
2017-02-27 16:55:45 +0100

    debut de la page inscription

--- inscription.php
+++ inscription.php
@@ -2,8 +2,20 @@
 $page = 'inscription';
 require_once('init.php');
 require_once('header.php');
- ?>
 
+if (!empty($_POST["user"]))
+{
+  if (isset($_POST['nom'])
+    && isset($_POST['prenom'])
+    && isset($_POST['email'])
+    && isset($_POST['password']))
+  {
+    
+  }
+}
+else
+{
+ ?>
  <form action="inscription.php" method="post">
    <input type="text" name="nom" placeholder="Votre Nom">
    <input type="text" name="prenom" placeholder="Votre Prenom">
@@ -11,3 +23,4 @@ require_once('header.php');
    <input type="password" name="mot de passe" placeholder="Votre mot de passe">
    <input type="submit" value="Banco !">
  </form>
+}

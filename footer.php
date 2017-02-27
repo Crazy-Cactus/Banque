96d424afcf52d1a2f02053351b17922a269ae987
Crazy_Cactus <Mathias.Genesis@Gmail.com>
2017-02-27 14:40:11 +0100

    update des bases

---
+++ footer.php
@@ -0,0 +1,7 @@
+
+<footer>
+  <small>les droits bla bla bla</small>
+</footer>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="icon" type="image/png" href="images/favicon.png" />
+    <link rel="stylesheet" href="css/style.css">
+    <title>Doge - Accueil</title>
+  </head>
+  <body>
+    <header>
+      <h1>Banque Doge</h1>
+    </header>
+
+    <main>

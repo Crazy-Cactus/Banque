748d1638b368cf78e959279c8f7f94bb91556c67
Crazy_Cactus <Mathias.Genesis@Gmail.com>
2017-02-28 09:14:31 +0100

    petits ajustements

--- inscription.php
+++ inscription.php
@@ -12,6 +12,7 @@ if (!empty($_POST['nom'])
   $prenom = htmlspecialchars($_POST['prenom']);
   $email = htmlspecialchars($_POST['email']);
   $password = htmlspecialchars($_POST['password']);
+
   $in_db = in_db('utilisateurs', 'email', $email);
 
   if (!$in_db)
@@ -19,7 +20,7 @@ if (!empty($_POST['nom'])
     $valid = insertion_user($nom, $prenom, $email, $password);
     if ($valid)
     {
-      header('location: index.php?message="SUCCESS"');
+      header('location: index.php?message=SUCCESS');
     }
     else
     {
@@ -30,10 +31,6 @@ if (!empty($_POST['nom'])
   {
     echo "cette adresse est deja utilisé";
   }
-}
-else
-{
-  echo "veuillez remplir tous les champs";
 }
  ?>
  <form action="inscription.php" method="post">
